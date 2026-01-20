a9ddbfdf41195a789224740c4e1dd570c111c348
jaepanman <65473635+jaepanman@users.noreply.github.com>
2026-01-20 13:22:50 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // CRITICAL: Ensures assets like audio and JS load correctly on GitHub Pages
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    emptyOutDir: true
   }
 });
